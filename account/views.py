0f4734ff74aa814ea4f08bdd7b0d35698963840c
Craig West <iwswordpress@gmail.com>
2024-06-16 12:59:22 +0100

    dashboard

--- account/views.py
+++ account/views.py
@@ -11,7 +11,7 @@ from payment.models import Order, OrderItem
 from django.contrib.auth.models import User
 
 from django.contrib.sites.shortcuts import get_current_site
-from . token import user_tokenizer_generate
+from .token import user_tokenizer_generate
 
 from django.template.loader import render_to_string
 from django.utils.encoding import force_bytes, force_str
@@ -27,17 +27,15 @@ from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 
 
-
-
 def register(request):
 
     form = CreateUserForm()
 
-    if request.method == 'POST':
+    if request.method == "POST":
 
         form = CreateUserForm(request.POST)
 
-        if form.is_valid(): 
+        if form.is_valid():
 
             user = form.save()
 
@@ -47,32 +45,26 @@ def register(request):
 
             # Email verification setup (template)
 
-            current_site = get_current_site(request)
+            # current_site = get_current_site(request)
 
-            subject = 'Account verification email'
+            # subject = 'Account verification email'
 
-            message = render_to_string('account/registration/email-verification.html', {
-            
-                'user': user,
-                'domain': current_site.domain,
-                'uid': urlsafe_base64_encode(force_bytes(user.pk)),
-                'token': user_tokenizer_generate.make_token(user),
-            
-            })
+            # message = render_to_string('account/registration/email-verification.html', {
 
-            user.email_user(subject=subject, message=message)
+            #     'user': user,
+            #     'domain': current_site.domain,
+            #     'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+            #     'token': user_tokenizer_generate.make_token(user),
 
+            # })
 
-            return redirect('email-verification-sent')
+            # user.email_user(subject=subject, message=message)
 
+            return redirect("email-verification-sent")
 
+    context = {"form": form}
 
-    context = {'form':form}
-
-
-    return render(request, 'account/registration/register.html', context=context)
-
-
+    return render(request, "account/registration/register.html", context=context)
 
 
 def email_verification(request, uidb64, token):
@@ -82,7 +74,7 @@ def email_verification(request, uidb64, token):
     unique_id = force_str(urlsafe_base64_decode(uidb64))
 
     user = User.objects.get(pk=unique_id)
-    
+
     # Success
 
     if user and user_tokenizer_generate.check_token(user, token):
@@ -91,62 +83,59 @@ def email_verification(request, uidb64, token):
 
         user.save()
 
-        return redirect('email-verification-success')
+        return redirect("email-verification-success")
 
-
-    # Failed 
+    # Failed
 
     else:
 
-        return redirect('email-verification-failed')
-
+        return redirect("email-verification-failed")
 
 
 def email_verification_sent(request):
 
-    return render(request, 'account/registration/email-verification-sent.html')
+    return render(request, "account/registration/email-verification-sent.html")
 
 
 def email_verification_success(request):
 
-    return render(request, 'account/registration/email-verification-success.html')
-
+    return render(request, "account/registration/email-verification-success.html")
 
 
 def email_verification_failed(request):
 
-    return render(request, 'account/registration/email-verification-failed.html')
-
+    return render(request, "account/registration/email-verification-failed.html")
 
 
 def my_login(request):
 
     form = LoginForm()
 
-    if request.method == 'POST':
+    if request.method == "POST":
 
         form = LoginForm(request, data=request.POST)
 
         if form.is_valid():
 
-            username = request.POST.get('username')
-            password = request.POST.get('password')
+            username = request.POST.get("username")
+            password = request.POST.get("password")
 
             user = authenticate(request, username=username, password=password)
 
             if user is not None:
+                print(user)
 
                 auth.login(request, user)
 
                 return redirect("dashboard")
 
+    context = {"form": form}
 
-    context = {'form':form}
-
-    return render(request, 'account/my-login.html', context=context)
+    return render(request, "account/my-login.html", context=context)
 
 
 # logout
+
 
 def user_logout(request):
 
@@ -154,7 +143,7 @@ def user_logout(request):
 
         for key in list(request.session.keys()):
 
-            if key == 'session_key':
+            if key == "session_key":
 
                 continue
 
@@ -162,36 +151,29 @@ def user_logout(request):
 
                 del request.session[key]
 
-
     except KeyError:
 
         pass
-
 
     messages.success(request, "Logout success")
 
     return redirect("store")
 
 
-
-
-@login_required(login_url='my-login')
+@login_required(login_url="my-login")
 def dashboard(request):
 
-
-    return render(request, 'account/dashboard.html')
-
+    return render(request, "account/dashboard.html")
 
 
-
-@login_required(login_url='my-login')
-def profile_management(request):    
+@login_required(login_url="my-login")
+def profile_management(request):
 
     # Updating our user's username and email
 
     user_form = UpdateUserForm(instance=request.user)
 
-    if request.method == 'POST':
+    if request.method == "POST":
 
         user_form = UpdateUserForm(request.POST, instance=request.user)
 
@@ -201,38 +183,31 @@ def profile_management(request):
 
             messages.info(request, "Update success!")
 
-            return redirect('dashboard')
+            return redirect("dashboard")
 
-   
+    context = {"user_form": user_form}
 
-    context = {'user_form':user_form}
-
-    return render(request, 'account/profile-management.html', context=context)
+    return render(request, "account/profile-management.html", context=context)
 
 
-
-
-@login_required(login_url='my-login')
+@login_required(login_url="my-login")
 def delete_account(request):
 
     user = User.objects.get(id=request.user.id)
 
-    if request.method == 'POST':
+    if request.method == "POST":
 
         user.delete()
 
-
         messages.error(request, "Account deleted")
 
+        return redirect("store")
 
-        return redirect('store')
-
-
-    return render(request, 'account/delete-account.html')
+    return render(request, "account/delete-account.html")
 
 
 # Shipping view
-@login_required(login_url='my-login')
+@login_required(login_url="my-login")
 def manage_shipping(request):
 
     try:
@@ -241,18 +216,15 @@ def manage_shipping(request):
 
         shipping = ShippingAddress.objects.get(user=request.user.id)
 
-
     except ShippingAddress.DoesNotExist:
 
         # Account user with no shipment information
 
         shipping = None
 
-
     form = ShippingForm(instance=shipping)
 
-
-    if request.method == 'POST':
+    if request.method == "POST":
 
         form = ShippingForm(request.POST, instance=shipping)
 
@@ -266,47 +238,28 @@ def manage_shipping(request):
 
             shipping_user.user = request.user
 
-
             shipping_user.save()
 
             messages.info(request, "Update success!")
 
-            return redirect('dashboard')
+            return redirect("dashboard")
+
+    context = {"form": form}
+
+    return render(request, "account/manage-shipping.html", context=context)
 
 
-    context = {'form':form}
-
-    return render(request, 'account/manage-shipping.html', context=context)
-
-
-
-
-
-@login_required(login_url='my-login')
+@login_required(login_url="my-login")
 def track_orders(request):
 
     try:
 
         orders = OrderItem.objects.filter(user=request.user)
 
-        context = {'orders':orders}
+        context = {"orders": orders}
 
-        return render(request, 'account/track-orders.html', context=context)
+        return render(request, "account/track-orders.html", context=context)
 
     except:
 
-        return render(request, 'account/track-orders.html')
-
-
- 
-
-
-
-
-
-
-
-
-
-
-
+        return render(request, "account/track-orders.html")
