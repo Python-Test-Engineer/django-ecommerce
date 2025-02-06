ce30f02030611ea82130191d3ba2d16c8e7ad108
Craig West <iwswordpress@gmail.com>
2025-02-06 14:15:35 +0000

    add namespace

--- core/urls.py
+++ core/urls.py
@@ -4,7 +4,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("products.urls")),
-    path("products/", include("products.urls", namespace="products")),
-    path("orders/", include("orders.urls", namespace="orders"))
+    path("", include("products.urls", namespace="products")),
+    path("orders/", include("orders.urls", namespace="orders")),
 ]
