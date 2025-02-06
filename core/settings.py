2b3d82c607347e527b1a37abcad300300df5f7ba
Craig West <iwswordpress@gmail.com>
2025-02-06 13:45:14 +0000

    edit settings

--- core/settings.py
+++ core/settings.py
@@ -20,9 +20,9 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["django-ecommerce-l0iy.onrender.com"]
 
-# CSRF_TRUSTED_ORIGINS = [""]
+CSRF_TRUSTED_ORIGINS = ["https://django-ecommerce-l0iy.onrender.com"]
 
 
 # Application definition
