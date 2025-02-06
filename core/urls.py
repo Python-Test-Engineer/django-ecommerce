81d3a461c957d5f6ef7449be5c24148efdb66604
Craig West <iwswordpress@gmail.com>
2025-02-06 14:12:41 +0000

    edit urls

--- core/urls.py
+++ core/urls.py
@@ -5,6 +5,6 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("products.urls")),
-    path("products/", include("products.urls")),
-    path("orders/", include("orders.urls")),
+    path("products/", include("products.urls", namespace="products")),
+    path("orders/", include("orders.urls", namespace="orders"))
 ]
